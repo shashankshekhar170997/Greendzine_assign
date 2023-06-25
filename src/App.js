619a5db5f36bd9d5a7bcc83f67ac67d966834689
shashank <shashankshekhar170997@gmail.com>
2023-06-25 11:44:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [items, setItem] = useState([]);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const response = await axios.get(
+          `https://reqres.in/api/users?page=${page}`
+        );
+        setItem(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchEmployees();
+  }, [page]);
+  const handlePage = (cPage) => {
+    setPage(cPage);
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const filterEmployees = items.filter((employee) =>
+    employee.first_name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="search first_name"
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="App">
+        {filterEmployees.map((employee) => (
+          <div key={employee.id}>
+            <img src={employee.avatar} alt={employee.first_name} />
+            <p>{employee.first_name}</p>
+          </div>
+        ))}
+        <div className="pagination">
+          <button onClick={() => handlePage(1)}>1</button>
+          <button onClick={() => handlePage(2)}>2</button>
+          <button onClick={() => handlePage(3)}>3</button>
+        </div>
+      </div>
+    </>
   );
 }
 
